12b360995e80794cc0d91ad157ed02f6ab311747
Ashish Dadhich <ashishthedadhich@gmail.com>
2023-12-20 12:33:22 +0530

    Added strategy design pattern to be followed for searching

--- playground/search/linearSearch.scala
+++ playground/search/linearSearch.scala
@@ -1,21 +1,41 @@
-def linearSearch[T](arr: Array[T], target: T): Option[Int] = {
-    for (i <- 0 until arr.length) {
-        if (arr(i) == target) {
-            return Some(i)
-        }
-    }
-    None
+// Strategy interface
+trait SearchStrategy[T] {
+    def search(arr: Array[T], target: T): Option[Int]
 }
 
-val intArray = Array(1, 2, 3, 4, 5)
-val intTarget = 3
+// Concrete strategy for linear search
+class LinearSearchStrategy[T] extends SearchStrategy[T] {
+    override def search(arr: Array[T], target: T): Option[Int] = {
+        for (i <- 0 until arr.length) {
+            if (arr(i) == target) {
+                return Some(i)
+            }
+        }
+        None
+    }
+}
 
-val intResult = linearSearch(intArray, intTarget)
+// Context class that uses the search strategy
+class SearchContext[T](strategy: SearchStrategy[T]) {
+    def performSearch(arr: Array[T], target: T): Option[Int] = {
+        strategy.search(arr, target)
+    }
+}
+
+// Usage
+val intArray = Array(1, 2, 3, 4, 5)
+val intTarget = 7
 
 val stringArray = Array("apple", "banana", "orange", "grape", "kiwi")
-val stringTarget = "orange"
+val stringTarget = "orangee"
 
-val stringResult = linearSearch(stringArray, stringTarget)
+val linearSearchStrategy = new LinearSearchStrategy[Int]()
+val intSearchContext = new SearchContext[Int](linearSearchStrategy)
+val intResult = intSearchContext.performSearch(intArray, intTarget)
+
+val stringSearchStrategy = new LinearSearchStrategy[String]()
+val stringSearchContext = new SearchContext[String](stringSearchStrategy)
+val stringResult = stringSearchContext.performSearch(stringArray, stringTarget)
 
 // Print results
 intResult match {
@@ -27,3 +47,4 @@ stringResult match {
     case Some(index) => println(s"Element $stringTarget found at index $index in the string array")
     case None => println(s"Element $stringTarget not found in the string array")
 }
+
